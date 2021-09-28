3edd9991342a304f1addb02edd7e17d4324c9c3b
KrishnaBNimbai <73700230+KrishnaBNimbai@users.noreply.github.com>
2021-09-28 19:59:01 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,12 @@
+package basic.java;
+public class OddEven {
+	public static void main(String[] args) {
+        int n = 77;
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+}
